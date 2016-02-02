00c8a56dafd7fb8a20c2568852799cfc480e80f4
AlekseyBilan <bilan.aleksey.sergeevich@gmail.com>
2016-02-02 14:33:25 +0200

    add project files. remove unused files and methods

--- js/todo.js
+++ js/todo.js
@@ -3,8 +3,6 @@
     var _data = [];
 
     function _addItem(name, duedate, description, completed) {
-        console.log(name, 'duedate = ', duedate, description, completed);
-        console.log(getCurrentId());
         if(!completed){
             completed = 'false';
         }
@@ -34,8 +32,6 @@
     }
 
     function _save() {
-        // второй параметр - функция, которая удаляет специальное свойство добавляемое angularJS для отслеживания дубликатов 
-        // http://mutablethought.com/2013/04/25/angular-js-ng-repeat-no-longer-allowing-duplicates/
         window.localStorage["tasks"] = JSON.stringify(_data, function (key, val) {
             if (key == '$$hashKey') {
                 return undefined;
@@ -46,7 +42,6 @@
 
     function _read() {
         var temp = window.localStorage["tasks"]
-        //{"id":1, "name":"adsvsdsvsdv", "description":"wdafawdfaf", "completed":true}
 
         if (!temp) _data = [];
         else _data = JSON.parse(temp);
@@ -69,13 +64,3 @@
     };
 
 })();
-/*
-
-
-    Задание 2
-Реализуйте SPA приложение, в котором пользователю представляется пройти тест из 5 вопросов, в каждом вопросе
- по 4 варианта ответа. Вопросы должны выбираться с помощью radio-button и когда выбран, например, вопрос №1,
- то на странице должны появится варианты ответа только для этого вопроса. В конце тестирования отобразите результат теста.
- При решении данной задачи используйте директиву ng-switch.
-
-  */
